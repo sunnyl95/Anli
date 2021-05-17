88d4cd91034cdea2b8c3dc9c947b52a61467b81a
孙慧玲 <sunhuiling@qianxin.com>
2021-05-17 19:39:48 +0800

    delete trackinfomining package

--- trackinfomining/trackinfomining/get_stay_home_data.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-
-def get_stay_home_data():
-    return pd.read_csv("./data/generate_data.csv")
--- trackinfomining/trackinfomining/get_stay_point_data.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-
-def get_stay_point_data():
-    return pd.read_csv("./data/traject.csv")
