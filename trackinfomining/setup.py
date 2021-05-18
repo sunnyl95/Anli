082be01bbd40e3fbd07ab3bca8b70729f11283e3
sunyiyi <sunnyl95@163.com>
2021-05-18 15:50:46 +0800

    version0.1

--- trackinfomining/setup.py
+++ trackinfomining/setup.py
@@ -9,7 +9,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
  
 setup(
     name='trackinfomining',
-    version=10.0,
+    version=0.1,
     description='trajectory information mining package',
     author='sunhuiling',
     author_email='sunnyl95@163.com',
